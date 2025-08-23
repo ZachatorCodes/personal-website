5e4dfa28903a4505954f8c914aa8ae76e326b74d
Zach Schwartz <zachatorcodes@gmail.com>
2025-08-22 18:03:22 -0600

    Fix issue with varying timings

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,45 +17,59 @@ function Home() {
     lg:text-3xl
   `;
 
-  useEffect(() => {
+  const words = [
+    "Computer Science Student",
+    "STEM Tutor",
+    "Outdoor Enthusiast",
+    "Full-Stack Developer",
+  ];
+
+  let word = 0;
+  let letters = words[word].split("");
+  let letter = 0;
+
+  function buildWord() {
     const element = document.getElementById("animated-text");
-    element.textContent = "Computer Science Student";
-    const words = [
-      "Computer Science Student",
-      "STEM Tutor",
-      "Outdoor Enthusiast",
-      "Full-Stack Developer"
-    ];
-    let word = 0;
-    let letters = words[word].split("");
-    let letter = 0;
-    const loopInterval = setInterval(() => {
-      if (element.textContent.length === 0) {
-        const addInterval = setInterval(() => {
-          if (element.textContent.length < letters.length) {
-            element.textContent = element.textContent += letters[letter];
-            letter++;
-          } else {
-            clearInterval(addInterval);
-          }
-        }, 100);
-      } else if (element.textContent.length === letters.length) {
-        letter = element.textContent.length - 1;
-        const deleteInterval = setInterval(() => {
-          if (element.textContent.length > 0) {
-            element.textContent = element.textContent.substring(0, letter - 1);
-            letter--;
-          } else {
-            if (word === words.length - 1) {
-              word = 0;
-            } else {
-              word++;
-            }
-            letters = words[word].split("");
-            clearInterval(deleteInterval);
-          }
-        }, 75);
+    const addInterval = setInterval(() => {
+      if (element.textContent.length < letters.length) {
+        element.textContent = element.textContent += letters[letter];
+        letter++;
+      } else {
+        clearInterval(addInterval);
+        setTimeout(() => {
+          destroyWord();
+        }, 2000);
       }
+    }, 200);
+  }
+
+  function destroyWord() {
+    const element = document.getElementById("animated-text");
+    letter = element.textContent.length - 1;
+    const deleteInterval = setInterval(() => {
+      if (element.textContent.length > 0) {
+        element.textContent = element.textContent.substring(0, letter - 1);
+        letter--;
+      } else {
+        if (word === words.length - 1) {
+          word = 0;
+        } else {
+          word++;
+        }
+        letters = words[word].split("");
+        clearInterval(deleteInterval);
+        setTimeout(() => {
+          buildWord();
+        }, 25);
+      }
+    }, 100);
+  }
+
+  useEffect(() => {
+    document.getElementById("animated-text").textContent =
+      "Computer Science Student";
+    setTimeout(() => {
+      destroyWord();
     }, 2000);
   }, []);
 
