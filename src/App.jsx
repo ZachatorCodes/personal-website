34c3f249ef11c70acf43b78de211f7005e53a387
Zach Schwartz <zachatorcodes@gmail.com>
2025-08-24 17:53:46 -0600

    Test reload on back arrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
 function App() {
   return (
     <div className="bg-base-200 h-screen">
