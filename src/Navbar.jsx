9a36840b33624ecf2d054a8f7b2e95ec596e5b40
Zach Schwartz <zachatorcodes@gmail.com>
2025-08-24 15:13:38 -0600

    Auto adjust navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,103 @@
+import { Link } from "react-router";
+
 function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-lg">
-      <div className="navbar-start"></div>
-      <div className="navbar-center">
+    <div className="navbar bg-base-300 shadow-md">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/membership">Membership</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <details>
+                <summary>Gear</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li>
+                    <Link to="/gear/basic">Basic</Link>
+                  </li>
+                  <li>
+                    <Link to="/gear/vertical">Vertical</Link>
+                  </li>
+                  <li>
+                    <Link to="/gear/survey">Survey</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl">
+          ZachatorCodes
+        </Link>
+      </div>
+      <div className="navbar-end hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/membership">Membership</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
           </li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>Gear</summary>
               <ul className="bg-base-100 rounded-t-none p-2">
                 <li>
-                  <a>Link 1</a>
+                  <Link to="/gear/basic">Basic</Link>
                 </li>
                 <li>
-                  <a>Link 2</a>
+                  <Link to="/gear/vertical">Vertical</Link>
+                </li>
+                <li>
+                  <Link to="/gear/survey">Survey</Link>
                 </li>
               </ul>
             </details>
           </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
         </ul>
       </div>
-      <div className="navbar-end"></div>
     </div>
   );
 }
