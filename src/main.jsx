7ca4e231ddd9dbb04eaed946a8470d6cdfc93f52
Zach Schwartz <zachatorcodes@gmail.com>
2025-08-24 15:26:30 -0600

    Create route to about page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import { createBrowserRouter, Navigate } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.jsx";
+import About from "./About.jsx";
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
       element: <App />,
+    },
+    {
+      path: "/about",
+      element: <About />
     },
     {
       path: "/*",
