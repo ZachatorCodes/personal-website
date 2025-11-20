e40fece371962685e08c172f350f31c6efb8928f
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-20 16:29:28 -0700

    Add routing to resume component

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./styles/index.css";
 import App from "./components/App.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
+import Resume from "./components/Resume.jsx";
 // import { StrictMode } from "react";
 
 const router = createBrowserRouter(
@@ -20,6 +21,10 @@ const router = createBrowserRouter(
     {
       path: "/projects",
       element: <Projects />
+    },
+    {
+      path: "/resume",
+      element: <Resume />
     },
     {
       path: "/*",
