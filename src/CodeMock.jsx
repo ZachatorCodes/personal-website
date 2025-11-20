32d88e9cba623b412f2e8853cd5d2d449c22f87b
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-20 00:04:00 -0700

    Move to class

--- src/CodeMock.jsx
+++ src/CodeMock.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
+import Timer from "./Timer";
 
 function CodeMock() {
+
   const mockup = `
     mockup-code
 
@@ -24,71 +26,13 @@ function CodeMock() {
     lg:text-3xl
   `;
 
-  const words = [
-    "Computer Science Student",
-    "STEM Tutor",
-    "Outdoor Enthusiast",
-    "Full-Stack Developer",
-  ];
-
-  let word = 0;
-  let letters = words[word].split("");
-  let letter = 0;
-
-  function cursorAnimation() {
-    const cursor = document.getElementById("cursor");
-    const blinkingAnimation = setInterval(() => {
-      cursor.className = "transition-opacity duration-300 opacity-0";
-      setTimeout(() => {
-        cursor.className = "transition-opacity duration-300 opacity-100";
-      }, 300);
-    }, 600);
-  }
-
-  function buildWord() {
-    const element = document.getElementById("animated-text");
-    const addInterval = setInterval(() => {
-      if (element.textContent.length < letters.length) {
-        element.textContent = element.textContent += letters[letter];
-        letter++;
-      } else {
-        clearInterval(addInterval);
-        setTimeout(() => {
-          destroyWord();
-        }, 2000);
-      }
-    }, 200);
-  }
-
-  function destroyWord() {
-    const element = document.getElementById("animated-text");
-    letter = element.textContent.length - 1;
-    const deleteInterval = setInterval(() => {
-      if (element.textContent.length > 0) {
-        element.textContent = element.textContent.substring(0, letter - 1);
-        letter--;
-      } else {
-        if (word === words.length - 1) {
-          word = 0;
-        } else {
-          word++;
-        }
-        letters = words[word].split("");
-        clearInterval(deleteInterval);
-        setTimeout(() => {
-          buildWord();
-        }, 25);
-      }
-    }, 100);
-  }
-
   useEffect(() => {
-    document.getElementById("animated-text").textContent =
-      "Computer Science Student";
-    cursorAnimation();
-    setTimeout(() => {
-      destroyWord();
-    }, 2000);
+    const timer = new Timer;
+    console.log(timer)
+
+    // return (
+    //   timer.endTimer()
+    // );
   }, []);
 
   return (
