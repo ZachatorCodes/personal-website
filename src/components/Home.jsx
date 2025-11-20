108d19dc30d2011a885711dd6b30ffe24e83ee11
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-20 15:15:54 -0700

    Move CSS style variables to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,44 @@
 import CodeMock from "./CodeMock";
+import headshot from "/headshot.JPEG";
 
 function Home() {
+  const mockup = `
+    mockup-code
+    w-90/100
+    lg:w-60/100
+    xl:w-50/100
+    shadow-2xl
+    ml-5
+  `;
+
+  const mainContent = `
+    flex
+    items-center
+    content-center
+    align-middle
+    justify-center
+    mt-9
+    wv
+    mt-6
+    lg:mt-9 
+    xl:mt-12 
+  `;
+
+  const imageStyle = `
+    h-75
+    w-75
+    rounded-full
+    object-cover
+    shadow-2xl
+    border-5
+    mr-5
+    hover-3d
+  `;
+
   return (
-    <div>
-      <CodeMock />
+    <div className={mainContent}>
+      <img src={headshot} className={imageStyle} />
+      <CodeMock mockup={mockup} />
     </div>
   );
 }
