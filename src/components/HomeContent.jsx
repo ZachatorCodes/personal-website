7148292e1cdeb24eb575e9c3919cf1f80c0c1ba5
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-24 20:58:18 -0700

    Change to dynamic cards

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,60 +1,48 @@
 function HomeContent() {
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className="px-10 pt-10">
+    <div className="flex">
+      <div className="card lg:card-side bg-base-100 shadow-sm">
+        <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+            alt="Album"
           />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
           </div>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className="px-10 pt-10">
+      <div className="card lg:card-side bg-base-100 shadow-sm">
+        <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+            alt="Album"
           />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
           </div>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className="px-10 pt-10">
+      <div className="card lg:card-side bg-base-100 shadow-sm">
+        <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+            alt="Album"
           />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
           </div>
         </div>
       </div>
